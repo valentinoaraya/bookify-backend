b788b707e1d5304faddd0fe19c0c1a6c431aca58
valentinoaraya <valentinoaraya04@gmail.com>
2025-03-17 18:11:49 -0300

    Cambio los emails de cancelación de turnos

--- src/utils/emailTextsAndHtmls.ts
+++ src/utils/emailTextsAndHtmls.ts
@@ -30,10 +30,12 @@ export const emailCancelAppointmentUser = (company: string, user: string, servic
     const htmlUser = `<h2>Turno cancelado</h2>
     <p>Hola ${user.split(" ")[0]},</p> 
     <p>El turno que tenías para <strong>${service}</strong> en <strong>${company}</strong> se ha cancelado correctamente.</p>
+    <p>En caso de que hayas abonado una seña para este turno, te hemos devuelto el 50% del dinero.</p>
     <p>El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.</p>`
 
     const textUser = `Hola ${user.split(" ")[0]},\n
     El turno que tenías para ${service} en ${company} se ha cancelado correctamente.\n
+    En caso de que hayas abonado una seña para este turno, te hemos devuelto el 50% del dinero.\n
     El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.`
 
     return { htmlUser, textUser }
@@ -43,10 +45,12 @@ export const emailCancelAppointmentCompany = (company: string, user: string, ser
     const htmlCompany = `<h2>Un turno ha sido cancelado</h2>
     <p>${company},</p>
     <p>El usuario <strong>${user}</strong> ha cancelado el turno que tenía para <strong>${service}</strong>.</p>
+    <p>En caso de que cobres una seña en este servicio, se le ha devuelto el 50% de la misma al usuario.</p>
     <p>El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.</p>`
 
     const textCompany = `${company},\n
     El usuario ${user} ha cancelado el turno que tenía para ${service}.\n
+    En caso de que cobres una seña en este servicio, se le ha devuelto el 50% de la misma al usuario.\n
     El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.`
 
     return { htmlCompany, textCompany }
@@ -56,10 +60,12 @@ export const emailDeleteAppointmentUser = (company: string, user: string, servic
     const htmlUser = `<h2>Tu turno ha sido cancelado</h2>
     <p>Hola ${user.split(" ")[0]},</p> 
     <p>El turno que tenías para <strong>${service}</strong> ha sido cancelado por <strong>${company}</strong>.</p>
+    <p>En caso de que hayas abonado una seña para este turno, te hemos devuelto la totalidad del dinero.</p>
     <p>El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.</p>`
 
     const textUser = `Hola ${user.split(" ")[0]},\n
     El turno que tenías para ${service} ha sido cancelado por ${company}.\n
+    En caso de que hayas abonado una seña para este turno, te hemos devuelto la totalidad del dinero.\n
     El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.`
 
     return { htmlUser, textUser }
@@ -69,10 +75,12 @@ export const emailDeleteAppointmentCompany = (company: string, user: string, ser
     const htmlCompany = `<h2>Turno cancelado</h2>
     <p>${company},</p>
     <p>El turno que <strong>${user}</strong> tenía para <strong>${service}</strong> ha sido cancelado correctamente.</p>
+    <p>En caso de que cobres una seña en este servicio, se le ha devuelto la totalidad del dinero al usuario.</p>
     <p>El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.</p>`
 
     const textCompany = `${company},\n
     El turno que ${user} tenía para ${service} ha sido cancelado correctamente.\n
+    En caso de que cobres una seña en este servicio, se le ha devuelto la totalidad del dinero al usuario.\n
     El turno estaba agendado para el día ${date} a las ${time} hs. Ahora el turno pasa a estar disponible nuevamente.`
 
     return { htmlCompany, textCompany }
